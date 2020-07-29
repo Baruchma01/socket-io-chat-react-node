c4dc050292494ccb824f9f37a6c2ddfb14a2c289
Baruch Mashasha <baruchma01@gmail.com>
2020-07-29 20:24:31 +0300

    style

--- client/src/containers/Chat/Chat.js
+++ client/src/containers/Chat/Chat.js
@@ -1,9 +1,58 @@
-import React from 'react'
+import React from "react";
+import classes from './Chat.css'
 
-const Chat = () => {
-    return (
-        <div>ChatWork</div>
-    )
-}
+const Chat = ({ location }) => {
+  const { username, room } = location.state.user;
 
-export default Chat
+  return (
+    <div className={classes.ChatContainer}>
+      <header className={classes.ChatHeader}>
+        <h1>
+          <i className="fas fa-smile"></i> ChatCord
+        </h1>
+        <a className={classes.Btn}>
+          Leave Room
+        </a>
+      </header>
+      <main className={classes.ChatMain}>
+        <div className={classes.ChatSideBar}>
+          <h3>
+            <i className="fas fa-comments"></i> Room Name:
+          </h3>
+          <h2 id="room-name">JavaScript</h2>
+          <h3>
+            <i className="fas fa-users"></i> Users
+          </h3>
+          <ul id="users">
+              <li>
+                  Baruch
+              </li>
+          </ul>
+        </div>
+        <div className={classes.ChatMessage}>
+          <div className={classes.Message}>
+            <p className={classes.Meta}>
+              UserName <span>12:30PM</span>
+            </p>
+            <p className="text">Hello World</p>
+          </div>
+        </div>
+      </main>
+      <div className={classes.ChatFormContainer}>
+        <form id="chat-form">
+          <input
+            id="msg"
+            type="text"
+            placeholder="Enter Message"
+            required
+          />
+          <button className={classes.Btn} onClick={(e) => e.preventDefault()}>
+            <i className="fas fa-paper-plane"></i> Send
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Chat;
--- client/src/containers/Join/Join.js
+++ client/src/containers/Join/Join.js
@@ -1,29 +1,65 @@
-import React from "react";
-import classes from './Join.css'
+import React, { useState } from "react";
+import classes from "./Join.css";
+import { useHistory } from "react-router-dom";
 
 const Join = () => {
+  const history = new useHistory();
+  const [joinFormField, setJoinFormField] = useState({
+    username: "",
+    room: "JavaScript",
+  });
+
+  const changeFormHandler = (e) => {
+    setJoinFormField({
+      ...joinFormField,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const joinChatHandler = (e) => {
+    e.preventDefault();
+    if (joinFormField.username === "") {
+      alert("אנא מלא את שדה השם");
+    } else {
+      history.push('/chat', {
+        user: joinFormField
+      });
+    }
+  };
+
+
+
   return (
     <div className={classes.JoinContainer}>
       <header className={classes.JoinHeader}>
         <h1>
-          <i className="fas fa-smile"></i> DeveloperChat
+          <i className="fas fa-comments"></i> DeveloperChat
         </h1>
       </header>
       <main className={classes.JoinMain}>
-        <form action="chat.html">
+        <form className={classes.Form}>
           <div className={classes.FormControl}>
-            <label className={classes.Label} htmlFor="username">Username</label>
-            <input className={classes.Input}
+            <label className={classes.Label}>NickName</label>
+            <input
+              className={classes.Input}
               type="text"
               name="username"
               id="username"
-              placeholder="Enter username..."
+              placeholder="Enter nickname..."
               required
+              value={joinFormField.userName}
+              onChange={changeFormHandler}
             />
           </div>
           <div className={classes.FormControl}>
-            <label className={classes.Label} htmlFor="room">Room</label>
-            <select className={classes.RoomSelect} name="room" id="room">
+            <label className={classes.Label}>Choose Room</label>
+            <select
+              className={classes.RoomSelect}
+              name="room"
+              id="room"
+              value={joinFormField.room}
+              onChange={changeFormHandler}
+            >
               <option value="JavaScript">JavaScript</option>
               <option value="Python">Python</option>
               <option value="PHP">PHP</option>
@@ -32,9 +68,7 @@ const Join = () => {
               <option value="Java">Java</option>
             </select>
           </div>
-          <button type="submit">
-            Join Chat
-          </button>
+          <button className={classes.JoinBtn} onClick={joinChatHandler}>Join Chat</button>
         </form>
       </main>
     </div>
