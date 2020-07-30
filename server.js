e8acbd0f6dbc0b46f918e43fe55e8492f9c4e755
Baruch Mashasha <baruchma01@gmail.com>
2020-07-30 23:44:56 +0300

    s

--- server.js
+++ server.js
@@ -71,6 +71,6 @@ io.on("connection", (socket) => {
   });
 });
 
-const PORT = 5000 || process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
