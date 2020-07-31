e2a05e4396e7dd80e7ef59ae19de9f20332a1a98
Baruch Mashasha <baruchma01@gmail.com>
2020-07-31 15:54:09 +0300

    new config

---
+++ client/build/precache-manifest.b160ac4a8609ddffae59b6a792bf0d5e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5404c2a1a0ea828ad805fc27df531127",
+    "url": "/index.html"
+  },
+  {
+    "revision": "495617f004c9445552cc",
+    "url": "/static/css/main.964f8988.chunk.css"
+  },
+  {
+    "revision": "3d1479a4c897391b228d",
+    "url": "/static/js/2.08a86a86.chunk.js"
+  },
+  {
+    "revision": "b67bc97fd0dd67004287af4028afc1b1",
+    "url": "/static/js/2.08a86a86.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "495617f004c9445552cc",
+    "url": "/static/js/main.d921ce81.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
