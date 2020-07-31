7cb11a4eb0ff824b6f9d7bf3020420574384f087
Baruch Mashasha <baruchma01@gmail.com>
2020-07-31 19:23:42 +0300

    spinner

--- client/src/containers/Chat/Chat.js
+++ client/src/containers/Chat/Chat.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react";
 import classes from "./Chat.css";
 import openSocket from "socket.io-client";
+import Spinner from "../../components/Spinner/Spinner";
+
 const socket = openSocket(process.env.REACT_APP_API_URI);
 const Chat = ({ location }) => {
   const [msg, setMsg] = useState("");
@@ -41,57 +43,64 @@ const Chat = ({ location }) => {
     setMsg(e.target.value);
   };
 
+  console.log(socket.connected);
   return (
-    <div className={classes.ChatContainer}>
-      <header className={classes.ChatHeader}>
-        <h1>
-          <i className="fas fa-smile"></i> ChatCord
-        </h1>
-        <a href="/" className={classes.Btn}>
-          Leave Room
-        </a>
-      </header>
-      <main className={classes.ChatMain}>
-        <div className={classes.ChatSideBar}>
-          <h3>
-            <i className="fas fa-comments"></i> Room Name:
-          </h3>
-          <h2 id="room-name">{currentRoom}</h2>
-          <h3>
-            <i className="fas fa-users"></i> Users
-          </h3>
-          <div id="users">
-            {(activeUsers || []).map((user) => (
-              <p key={user.id}>{user.username}</p>
-            ))}
+    <>
+      {!socket.connected ? (
+        <Spinner />
+      ) : (
+        <div className={classes.ChatContainer}>
+          <header className={classes.ChatHeader}>
+            <h1>
+              <i className="fas fa-smile"></i> ChatCord
+            </h1>
+            <a href="/" className={classes.Btn}>
+              Leave Room
+            </a>
+          </header>
+          <main className={classes.ChatMain}>
+            <div className={classes.ChatSideBar}>
+              <h3>
+                <i className="fas fa-comments"></i> Room Name:
+              </h3>
+              <h2 id="room-name">{currentRoom}</h2>
+              <h3>
+                <i className="fas fa-users"></i> Users
+              </h3>
+              <div id="users">
+                {(activeUsers || []).map((user) => (
+                  <p key={user.id}>{user.username}</p>
+                ))}
+              </div>
+            </div>
+            <div className={classes.ChatMessage}>
+              {(roomMessage || []).map((message) => (
+                <div key={Math.random()} className={classes.Message}>
+                  <p className={classes.Meta}>
+                    {message.username} <span>{message.time}</span>
+                  </p>
+                  <p className="text">{message.text}</p>
+                </div>
+              ))}
+            </div>
+          </main>
+          <div className={classes.ChatFormContainer}>
+            <form id="chat-form">
+              <input
+                id="msg"
+                type="text"
+                placeholder="Enter Message"
+                value={msg}
+                onChange={handleMsgChange}
+              />
+              <button className={classes.Btn} onClick={submitMessageHandler}>
+                <i className="fas fa-paper-plane"></i> Send
+              </button>
+            </form>
           </div>
         </div>
-        <div className={classes.ChatMessage}>
-          {(roomMessage || []).map((message) => (
-            <div key={Math.random()} className={classes.Message}>
-              <p className={classes.Meta}>
-                {message.username} <span>{message.time}</span>
-              </p>
-              <p className="text">{message.text}</p>
-            </div>
-          ))}
-        </div>
-      </main>
-      <div className={classes.ChatFormContainer}>
-        <form id="chat-form">
-          <input
-            id="msg"
-            type="text"
-            placeholder="Enter Message"
-            value={msg}
-            onChange={handleMsgChange}
-          />
-          <button className={classes.Btn} onClick={submitMessageHandler}>
-            <i className="fas fa-paper-plane"></i> Send
-          </button>
-        </form>
-      </div>
-    </div>
+      )}
+    </>
   );
 };
 
