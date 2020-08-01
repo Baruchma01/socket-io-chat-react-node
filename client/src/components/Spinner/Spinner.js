4badab1369565554b9825fec00594a709b0599ee
Baruch Mashasha <baruchma01@gmail.com>
2020-08-01 17:34:46 +0300

    loader

--- client/src/components/Spinner/Spinner.js
+++ client/src/components/Spinner/Spinner.js
@@ -1,9 +1,24 @@
-import React, { Fragment } from 'react';
-import spinner from './spinner.gif'
+import React from "react";
+import spinner from "./spinner.gif";
 
-export default ()=>(
-    <Fragment>
-        <img src={spinner} style={{width:'200px',margin:'auto', marginTop: '15rem',display:'block'}}
-        alt='Loading...'/>
-    </Fragment>
-)
+export default () => (
+  <div
+    style={{
+      width: "500px",
+      margin: "100px auto",
+      display: "flex",
+      flexDirection: "column",
+      justifyContent: "center",
+      alignItems: "center",
+    }}
+  >
+    <h1 style={{color:'cornflowerblue'}}>Sorry for the slow loading ... this is a free Heroku server :)</h1>
+    <img
+      src={spinner}
+      style={{
+        width: "200px",
+      }}
+      alt="Loading..."
+    />
+  </div>
+);
